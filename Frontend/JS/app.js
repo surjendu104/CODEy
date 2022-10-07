704770b387c33d8bf4f24379be146d82d4aefee6
surjendu104 <cyber00daddy@gmail.com>
2022-10-07 18:52:37 +0530

    07.10.2022

---
+++ Frontend/JS/app.js
@@ -0,0 +1,17 @@
+let editor
+
+window.onload = function() {
+    editor = ace.edit("editor")
+    editor.setTheme("ace/theme/monokai")
+    editor.session.setMode("ace/mode/c_cpp")
+}
+
+function changeLanguage() {
+    let language = $("#languages").val();
+
+    if(language=='c' || language=='cpp')editor.session.setMode("ace/mode/c_cpp")
+    else if(language=='php')editor.session.setMode("ace/mode/php")
+    else if(language=='python')editor.session.setMode("ace/mode/python")
+    else if(language=='node')editor.session.setMode("ace/mode/javascript")
+}
+console.log("HI")
