bc93855bcd937ee80af667201686f677a6e0cf67
surjendu104 <cyber00daddy@gmail.com>
2022-10-09 21:01:30 +0530

    Updating Some Functions

--- Frontend/JS/app.js
+++ Frontend/JS/app.js
@@ -1,8 +1,28 @@
-let editor
+let editor = document.querySelector("#editor")
 
-window.onload = function() {
+ace.edit(editor, {
+    mode:"ace/mode/c_cpp",
+    selectionStyle: "text",
+    theme: "ace/theme/cobalt",
+    fontSize:'15px',
+    vScrollBarAlwaysVisible: true
+})
+
+window.onload = ()=> {
     editor = ace.edit("editor")
-    editor.setTheme("ace/theme/monokai")
     editor.session.setMode("ace/mode/c_cpp")
 }
-console.log("HI")
+
+function changeLanguage() {
+    let lang = document.getElementById('language')
+
+    let language = lang.options[lang.selectedIndex].value
+    console.log(language)
+    if(language=='c' || language=='cpp')editor.session.setMode('ace/mode/c_cpp');
+    else if(language=='java')editor.session.setMode('ace/mode/java');
+    else if(language=='python')editor.session.setMode('ace/mode/python');
+    else if(language=='php')editor.session.setMode('ace/mode/php');
+    else if(language=='javascript')editor.session.setMode('ace/mode/javascript');
+}
+
+
